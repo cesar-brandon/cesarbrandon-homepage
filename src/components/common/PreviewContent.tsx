cd49a5f9d8e75f305001a458f56403968c63b907
Cesar Brandon <cesarbrandon000@gmail.com>
2023-10-07 19:04:27 -0500

    :lipstick: Change in project submission

--- src/components/common/PreviewContent.tsx
+++ src/components/common/PreviewContent.tsx
@@ -1,6 +1,7 @@
 import urlFor from "@/lib/urlFor";
 import React from "react";
 import BlurImage from "./blur-image";
+import ButtonLink from "./button-link";
 
 type Props = {
   post: Post;
@@ -9,29 +10,26 @@ type Props = {
 const PreviewContent: React.FC<Props> = ({ post }) => {
   return (
     <div
-      className="relative w-full h-80 drop-shadow-xl
-					rounded-lg overflow-hidden group"
-    >
-      <BlurImage
-        className="object-cover object-center"
-        src={urlFor(post.mainImage).url()}
-        alt={post.author.name}
-        fill
-      />
+      className="relative w-full h-80 shadow-md
+			rounded-lg overflow-hidden group border border-black border-opacity-10">
+      <div className="relative w-full h-[80%] group-hover:h-[70%] transition-all duration-300 overflow-hidden">
+        <BlurImage
+          className="object-cover object-center"
+          src={urlFor(post.mainImage).url()}
+          alt={post.author.name}
+          fill
+        />
+        <div className="absolute bottom-0 left-0 h-28 group-hover:h-36 w-full 
+          bg-gradient-to-t from-white dark:from-zinc-900 to-transparent transition-all duration-300"></div>
+      </div>
       <div
-        className="absolute bottom-0 w-full h-16
-				bg-gray-800 backdrop-filter backdrop-blur-sm 
-				bg-opacity-50 flex items-center pl-4 transition-all duration-300
-				opacity-0 group-hover:opacity-100"
-      >
-        {post.topics.map((topic) => (
-          <span
-            key={topic.title}
-            className="text-xs text-white bg-gray-900 px-2 py-1 rounded-md"
-          >
-            {topic.title}
-          </span>
-        ))}
+        className="absolute flex flex-col gap-2 h-28 bottom-0 
+				bg-opacity-50 px-8 pb-8 transition-all duration-300 group-hover:-translate-y-16">
+        <p className="text-2xl font-bold">{post.title}</p>
+        <p>Lorem ipsum dolor sit amet, qui minim labore.Lorem ipsum dolor sit amet, officia.</p>
+        <ButtonLink className="mt-4" href={`/projects/${post.slug.current}`}
+          text="Read More" ariaLabel="Read More"
+          icon={<p className="font-mono ml-2">{"~>"}</p>} />
       </div>
     </div>
   );
